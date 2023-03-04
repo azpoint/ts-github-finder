abb47e294a7647799c30c9d7137550d5ed9cea9e
Alejandro Zapata <alejo8708@gmail.com>
2023-03-04 14:35:09 +0100

    starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
+// Dependencies
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+
+//Components
+import Navbar from './components/layaout/Navbar';
+import Footer from './components/layaout/Footer';
+
+//Componets --- Pages
+import About from "./pages/About"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="flex flex-col justify-between h-screen">
+      <Navbar title='TS Github Finder'/>
+
+      <main className='container mx-auto px-3 pb-12'>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/not-found" element={<NotFound />}/>
+          <Route path="/*" element={<NotFound />}/>
+        </Routes>
+      </main>
+
+      <Footer />
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/layaout/Navbar.tsx
@@ -0,0 +1,36 @@
+//Dependencies
+import { Link } from "react-router-dom";
+
+//Components
+import { FaGithub } from "react-icons/fa";
+
+type NavbarProps = {
+  title: string;
+};
+
+const Navbar: React.FC<NavbarProps> = ({ title }) => {
+  return (
+    <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
+      <div className="container mx-auto">
+        <div className="flex-none px-2 mx-2">
+            <FaGithub className="inline pr-2 text-4xl"/>
+            <Link to="/" className="text-lg font-bold align-middle">
+                {title}
+            </Link>
+        </div>
+        <div className="flex-1 px-2 mx-2">
+            <div className="flex justify-end">
+                <Link to="/" className="btn btn-ghost btn-sm rouded-btn">
+                    Home
+                </Link>
+                <Link to="/about" className="btn btn-ghost btn-sm rouded-btn">
+                    About
+                </Link>
+            </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
